d21f209b8fef2932ec7b2ace9006d831c98d9c1f
ornob011 <ornob011@gmail.com>
2023-10-30 18:19:57 +0600

    feat: add test file

---
+++ src/test/java/com/example/phpreplica/PhpreplicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phpreplica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhpreplicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
