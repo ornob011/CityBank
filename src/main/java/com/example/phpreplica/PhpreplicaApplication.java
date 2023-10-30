088ba1905e6041f5cdd79697b01009dffd52d15a
ornob011 <ornob011@gmail.com>
2023-10-30 18:20:02 +0600

    feat: add main file

---
+++ src/main/java/com/example/phpreplica/PhpreplicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.phpreplica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhpreplicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhpreplicaApplication.class, args);
+	}
+
+}
